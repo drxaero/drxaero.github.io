8d46463d1aed194f7160eaadaaf94ae650c2c961
Janus Cheng <drxaero@gmail.com>
2025-01-20 11:05:35 +0800

    NEW_FEATURE - Introduced support to dark mode toggling.

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const htmlElement = document.documentElement;
+    const switchElement = document.getElementById('color_mode_switch_id');
+
+    // Set the default theme to dark if no setting is found in local storage
+    const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+    switchElement.checked = currentTheme === 'light';
+
+    switchElement.addEventListener('change', function () {
+        if (this.checked) {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('bsTheme', 'light');
+        } else {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('bsTheme', 'dark');
+        }
+    });
+});
